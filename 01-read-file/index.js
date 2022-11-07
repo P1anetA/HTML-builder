d662420c1acea0ffd1352a75f2669e8bca83cb62
P1anetA <natkoch09@gmail.com>
2022-11-07 23:41:45 +0300

    fix: add ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,4 @@
 const path = require('path');
 const fs = require('fs');
-fs.readFile(
-  path.join(__dirname, 'text.txt'),
-  'utf-8',
-  (err, data) => {
-      if (err) throw err;
-      console.log(data);
-  }
-);
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('data', el => console.log(el));
