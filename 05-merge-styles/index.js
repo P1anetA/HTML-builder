7d938043dd7fba297ea71c6001902e3059d6b30e
P1anetA <natkoch09@gmail.com>
2022-11-08 12:23:57 +0300

    fix: fixed result

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,13 +5,15 @@ const styles = path.resolve(__dirname, 'styles');
 // const resultPath = path.resolve(__dirname, 'project-dist');
 const bundle = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'bundle.css'));
 
-async () => {
+let newFunc = async () => {
   const arr = await fsPromises.readdir(styles, { withFileTypes: true });
   arr.forEach(style => {
     const ext = path.extname(style.name);
-    if (style.isFile() && ext === '.css') {
+    if (style.isFile() && ext == '.css') {
       let compiler = fs.createReadStream(path.resolve(styles, style.name));
       compiler.pipe(bundle);
     }
   });
-}
+};
+
+newFunc();
